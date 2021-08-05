4d2f448b0e9a59e52902403d70897212c23ccfb2
Rustam <rustamaaaa@bk.ru>
2021-08-05 18:47:39 +0300

    fix/Указал верный адрес сервера

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 // Для каждого отдельного сервера, свой apiServe объект
 const apiServeMesto = {
-  CONECT_SERVER: 'http://mestobackend.nomoredomains.club',
+  CONECT_SERVER: 'https://mestobackend.nomoredomains.club',
   PATHS: {
     user: 'users/me',
     cards: 'cards',
@@ -9,7 +9,7 @@ const apiServeMesto = {
 };
 
 const apiServeAuth = {
-  CONECT_SERVER: 'http://mestobackend.nomoredomains.club',
+  CONECT_SERVER: 'https://mestobackend.nomoredomains.club',
   PATHS: {
     reg: 'signup',
     login: 'signin',
