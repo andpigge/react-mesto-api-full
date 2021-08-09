d752b191452ec24e0473b99e36c291fb6ef41b03
Rustam <rustamaaaa@bk.ru>
2021-08-09 15:11:12 +0300

    fix/Перенес стили валидации в отдельный файл

--- frontend/src/components/mesto/main/popups/AddPlacePopup.js
+++ frontend/src/components/mesto/main/popups/AddPlacePopup.js
@@ -6,6 +6,14 @@ import { LogicsAllPopups } from '../../../../contexts/logicsAllPopups';
 
 // Валидация
 import validateString from '../../../../utils/validate/validateString';
+import {
+  spanStylesValidateTrue,
+  spanStylesValidateFalse,
+  inputStylesValidateTrue,
+  inputStylesValidateFalse,
+  buttonStylesValidateTrue,
+  buttonStylesValidateFalse,
+} from '../../../../utils/validate/styles';
 
 function AddPlacePopup({ onAddPlace, loading }) {
   const [placeName, setPlaceName] = useState('');
@@ -49,24 +57,51 @@ function AddPlacePopup({ onAddPlace, loading }) {
   };
 
   return (
-    <PopupWithForm title={'Новое место'} name={'popup_add_card'} isOpen={isOpen} onClose={resetFormFieldsOnClose} onSubmit={handleSubmit}>
+    <PopupWithForm
+      title={'Новое место'}
+      name={'popup_add_card'}
+      isOpen={isOpen}
+      onClose={resetFormFieldsOnClose}
+      onSubmit={handleSubmit}
+    >
       <>
         <label className="popup__form-label">
-          <span style={{color: isValid ? '#167c16' : '#d43d3d'/* , display: isValid ? 'none' : 'block' */ }} >
+          <span style={ isValid ? spanStylesValidateTrue : spanStylesValidateFalse } >
             *
           </span>
-          <input type="text" className="popup__form-input popup__form-input_value_place" id="place-name-input" placeholder="Название" name="placeName" minLength="2" maxLength="30" required value={placeName} onChange={checkInputName}
-          style={{borderColor: isValid ? 'rgba(0, 0, 0, 0.2)' : '#d43d3d' }} />
-          <span style={{color: isValid ? '#167c16' : '#d43d3d' }}>
+          <input
+            type="text"
+            className="popup__form-input popup__form-input_value_place"
+            id="place-name-input" placeholder="Название"
+            name="placeName"
+            minLength="2"
+            maxLength="30"
+            required value={placeName}
+            onChange={checkInputName}
+            style={ isValid ? inputStylesValidateTrue : inputStylesValidateFalse }
+          />
+          <span style={isValid ? spanStylesValidateTrue : spanStylesValidateFalse }>
             {
               isValid ? messageInput.message : messageInput.error
             }
           </span>
         </label>
         <label className="popup__form-label">
-          <input type="url" className="popup__form-input popup__form-input_value_img" placeholder="Ссылка на картинку" id="place-img-input" name="placeImg" required value={placeImg} onChange={e => setPlaceImg(e.target.value)} />
+          <input
+            type="url"
+            className="popup__form-input popup__form-input_value_img"
+            placeholder="Ссылка на картинку"
+            id="place-img-input"
+            name="placeImg"
+            required value={placeImg}
+            onChange={e => setPlaceImg(e.target.value)}
+          />
         </label>
-        <button className="button-popup button-popup_add_card" type="submit" disabled={!isValid} style={{backgroundColor: isValid ? 'black' : 'rgba(0, 0, 0, .3)' }} >
+        <button
+          className="button-popup button-popup_add_card"
+          type="submit" disabled={!isValid}
+          style={ isValid ? buttonStylesValidateTrue : buttonStylesValidateFalse }
+        >
           {loading ? 'Создать...' : 'Создать'}
         </button>
       </>
---
+++ frontend/src/utils/validate/styles.js
@@ -0,0 +1,32 @@
+const spanStylesValidateTrue = {
+  color: '#167c16',
+};
+
+const spanStylesValidateFalse = {
+  color: '#d43d3d',
+};
+
+const inputStylesValidateTrue = {
+  borderColor: 'rgba(0, 0, 0, 0.2)',
+};
+
+const inputStylesValidateFalse = {
+  borderColor: '#d43d3d',
+};
+
+const buttonStylesValidateTrue = {
+  backgroundColor: 'black',
+};
+
+const buttonStylesValidateFalse = {
+  backgroundColor: 'rgba(0, 0, 0, .3)',
+};
+
+export {
+  spanStylesValidateTrue,
+  spanStylesValidateFalse,
+  inputStylesValidateTrue,
+  inputStylesValidateFalse,
+  buttonStylesValidateTrue,
+  buttonStylesValidateFalse,
+};
